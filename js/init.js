f7d0e4af9f57293f1ff455904c2a006a55ddb668
Bruno de araujo Bisogni <brunobisogni14@gmail.com>
2020-01-07 00:06:43 -0300

    mais texto

--- js/init.js
+++ js/init.js
@@ -125,7 +125,8 @@ var penelope = function() {
 	
 	/*Typed js start*/
 	$("#typed").typed({
-		strings: ["^2100&nbsp;I Am Abe.<br/>UI / UX Developer.<br/> Living in New York City."],
+		strings: ["^2100&nbsp;I Am Bruno.<br/>UI / Desenvolvedor Full Stack JS e Gerente de projetos.
+		<br/> Moro em São Paulo."],
 		typeSpeed: 30,
 		backDelay: 750,
 		loop: false,
